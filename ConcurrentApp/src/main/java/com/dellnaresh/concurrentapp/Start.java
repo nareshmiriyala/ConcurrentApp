cf094398a0685c288b4f6f491e758c08f37b1970
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2014-11-27 22:32:19 +1100

    Updating Code

--- ConcurrentApp/src/main/java/com/dellnaresh/concurrentapp/Start.java
+++ ConcurrentApp/src/main/java/com/dellnaresh/concurrentapp/Start.java
@@ -1,9 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dellnaresh.concurrentapp;
+
+import com.dellnaresh.entity.Trans;
+import com.dellnaresh.jpa.TransJpaController;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.metamodel.Metamodel;
+import java.util.Map;
 
 /**
  *
@@ -15,7 +18,11 @@ public class Start {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("com.dellnaresh_ConcurrentApp_jar_1.0-SNAPSHOTPU");
+       TransJpaController transJpaController=new TransJpaController(entityManagerFactory);
+//        transJpaController.create(new Trans());
+        System.out.println(transJpaController.getTransCount());
+
     }
-    
+
 }
