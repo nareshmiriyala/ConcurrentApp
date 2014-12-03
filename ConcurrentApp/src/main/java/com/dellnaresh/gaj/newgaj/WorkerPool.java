258557f8a821a24cbe7e97d0d7582a549f92ca72
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2014-12-03 20:11:32 +1100

    Worked Pool

--- ConcurrentApp/src/main/java/com/dellnaresh/gaj/newgaj/WorkerPool.java
+++ ConcurrentApp/src/main/java/com/dellnaresh/gaj/newgaj/WorkerPool.java
@@ -1,37 +1,48 @@
 package com.dellnaresh.gaj.newgaj;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class WorkerPool {
+    private static final int corePoolSize = 3;
+    private static final int maximumPoolSize = 5;
+    private static final long keepAliveTimeInMinutes = 60;
   private static ThreadPoolExecutor executorPool ;
     private static MyMonitorThread monitor;
-    public static void main(String args[]) throws InterruptedException{
-        //RejectedExecutionHandler implementation
-        RejectedExecutionHandlerImpl rejectionHandler = new RejectedExecutionHandlerImpl();
+    private volatile static WorkerPool workerPool = null;
+
+    private WorkerPool() {
+
+    }
+
+    public static WorkerPool getInstance() {
+        if (workerPool == null) {
+            synchronized (WorkerPool.class) {
+                if (workerPool == null) {
+                    workerPool = new WorkerPool();
+                    try {
+                        workerPool.createWorkerPool();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return workerPool;
+    }
+
+    private static void createWorkerPool() throws InterruptedException {
         //Get the ThreadFactory implementation to use
         ThreadFactory threadFactory = Executors.defaultThreadFactory();
         //creating the ThreadPoolExecutor
-        executorPool = new ThreadPoolExecutor(2, 5, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2), threadFactory);
-        executorPool.setRejectedExecutionHandler(
-                new ThreadPoolExecutor.CallerRunsPolicy());
+        executorPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTimeInMinutes, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(2), threadFactory);
+        executorPool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         //start the monitoring thread
         monitor = new MyMonitorThread(executorPool, 3);
         Thread monitorThread = new Thread(monitor);
         monitorThread.start();
-        //submit work to the thread pool
-        for(int i=0; i<10; i++){
-            deployJob( i);
-        }
-        shutdown();
-
-
     }
 
-    private static void shutdown() throws InterruptedException {
+    public static void shutdown() throws InterruptedException {
         //shut down the pool
 //        Thread.sleep(30000);
         executorPool.shutdown();
@@ -42,7 +53,8 @@ public class WorkerPool {
 
     }
 
-    private static void deployJob( int i) {
-        executorPool.submit(new WorkerThread("cmd"+i));
+    public static void deployJob(AbstractJob job) {
+
+        executorPool.submit(job);
     }
 }
