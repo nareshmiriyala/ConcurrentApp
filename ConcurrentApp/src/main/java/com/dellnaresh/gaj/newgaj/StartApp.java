98cc84c30d62a00af19f887bdcab66101d66bea0
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2014-12-03 20:12:13 +1100

    Worked Pool

---
+++ ConcurrentApp/src/main/java/com/dellnaresh/gaj/newgaj/StartApp.java
@@ -0,0 +1,24 @@
+package com.dellnaresh.gaj.newgaj;
+
+/**
+ * Created by nareshm on 3/12/2014.
+ */
+public class StartApp {
+    public static void main(String[] args) {
+        WorkerPool workerPool = WorkerPool.getInstance();
+        AbstractJob job = new WorkerThread("naresh");
+        workerPool.deployJob(job);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        AbstractJob job1 = new WorkerThread("suresh");
+        workerPool.deployJob(job1);
+        try {
+            workerPool.shutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
